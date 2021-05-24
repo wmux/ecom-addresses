db5fae6997c034c1f399f19aeaef3be12e9544d0
Justin Alvarado <84186454+wmux@users.noreply.github.com>
2021-05-24 09:47:42 -0500

    Pull Addresses from Airtable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: "Ecom Addresses",
@@ -23,6 +25,25 @@ module.exports = {
         path: "./src/images/",
       },
       __key: "images",
+    },
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.AIRTABLE_API_KEY, // may instead specify via env, see below
+        concurrency: 5, // default, see using markdown and attachments for more information
+        tables: [
+          {
+            baseId: `app68EicO4fuMxV48`,
+            tableName: `Residential`,
+            queryName: `resi`, // optionally default is false - makes all records in this table a separate node type, based on your tableView, or if not present, tableName, e.g. a table called "Fruit" would become "allAirtableFruit". Useful when pulling many airtables with similar structures or fields that have different types. See https://github.com/jbolda/gatsby-source-airtable/pull/52.
+          },
+          {
+            baseId: `app68EicO4fuMxV48`,
+            tableName: `Commercial`,
+            queryName: `smb`, // optionally default is false - makes all records in this table a separate node type, based on your tableView, or if not present, tableName, e.g. a table called "Fruit" would become "allAirtableFruit". Useful when pulling many airtables with similar structures or fields that have different types. See https://github.com/jbolda/gatsby-source-airtable/pull/52.
+          },
+        ],
+      },
     },
   ],
 };
